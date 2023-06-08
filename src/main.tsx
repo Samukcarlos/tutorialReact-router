9dae8fbe6f7a56f07c08c361994e39fb34a375b9
Samuk <samukcarlos@gmail.com>
2023-06-08 19:14:45 -0300

    Criado Tutorial de React Rotas

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import App from './App'
+import Expenses from './routes/Expenses'
+import Invoices from './routes/Invoices'
+import Invoice from './routes/Invoices/Invoice'
+import InvoicesIndex from './routes/Invoices/Invoice/index'
+import NotFound from './routes/NotFound'
+import Welcome from './routes/Welcome'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Welcome />} />
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />}>
+          <Route index element={<InvoicesIndex />} />
+          <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
+        <Route path="*" element={<NotFound />}
+        />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 )
